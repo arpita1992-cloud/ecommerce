3cb4e6c988a73898794efc816fb11ae13666c62d
arpita1992-cloud <arpitakabir8@gmail.com>
2020-06-24 23:44:47 +0600

    upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+/*var abc =document.getElementById('abc');
+abc.onclick=function(){
+//document.getElementById('paragraph').innerHTML="To become the premier choice for corporate and social events for event planning";
+window.location.href="productdetails.html";
+document.getElementById('paragraph').value;
+}*/
+
+var GoalsBtn =document.getElementById('GoalsBtn');
+GoalsBtn.onclick=function(){
+document.getElementById('paragraph').innerHTML="bjhxsdsmlllllllmnkdncdsmfdslllllllllllllkhfhjdshjdsg";
+
+
+}
